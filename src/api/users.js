2a196e4efdbf29d9aec556469015edf4082fd582
Anton <office.sklyarov@gmail.com>
2020-12-07 18:15:16 +0200

    update request func's

--- src/api/users.js
+++ src/api/users.js
@@ -4,12 +4,5 @@ export async function getUsers() {
   const response = await fetch(`${BASE_URL}/users`);
   const result = await response.json();
 
-  const formatedUsers = result.map(user => (
-    {
-      ...user,
-      id: Number(user.id),
-    }
-  ));
-
-  return formatedUsers;
+  return result;
 }
